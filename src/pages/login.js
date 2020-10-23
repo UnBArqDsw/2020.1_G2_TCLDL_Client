1b94c39efafc1e86b4274e686d4a77b41f5038cd
Guilherme Deusdará <guibanci@gmail.com>
2020-10-22 23:06:03 -0300

    Adding NavBar to home and login
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Mikhaelle <mikhabueno@outlook.com>

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,33 +1,37 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import AppBar from '../components/AppBar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Login Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <AppBar />
+      <div className={styles.container}>
+        <Head>
+          <title>Login Page</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to
-          {' '}
-          <a href="https://nextjs.org">Login Page</a>
-        </h1>
-      </main>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to
+            {' '}
+            <a href="https://nextjs.org">Login Page</a>
+          </h1>
+        </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          {' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by
+            {' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
