9090e88c4ae45db9678f6c9bf8d35ee1bf37c20f
Guilherme Deusdará <guibanci@gmail.com>
2020-10-24 15:34:03 -0300

    ajusting Sign Component
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Mikhaelle <mikhabueno@outlook.com>

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -1,36 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import AppBar from '../components/AppBar'
+import SignComponent from '../components/Sign'
 
 export default function Home() {
   return (
     <div>
       <AppBar />
-      <div className={styles.container}>
-        <Head>
-          <title>TCLDL</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to
-            {' '}
-            <a href="https://nextjs.org">Createaa aaaAccount!</a>
-          </h1>
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by
-            {' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
-      </div>
+      <SignComponent />
     </div>
   );
 }
