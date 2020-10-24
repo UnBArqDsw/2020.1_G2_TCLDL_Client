571357f46ed8d559a80f2401580ad8a3da8df4b2
Guilherme Deusdará <guibanci@gmail.com>
2020-10-24 16:20:46 -0300

    Ajusting create account page
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Mikhaelle <mikhabueno@outlook.com>

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -1,13 +1,28 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import useStyles from '../styles/CreateAccount.module';
+
 import AppBar from '../components/AppBar'
 import SignComponent from '../components/Sign'
-
 export default function Home() {
+  const styles = useStyles();
+
   return (
     <div>
       <AppBar />
-      <SignComponent />
+      <SignComponent>
+        <form className={styles.form} noValidate autoComplete="off">
+          <TextField className={styles.field} id="standard-basic" label="Username" variant="outlined" />
+          <TextField className={styles.field} id="filled-basic" label="Email" variant="outlined" />
+          <TextField className={styles.field} id="outlined-basic" label="Password" variant="outlined" />
+          <TextField className={styles.field} id="outlined-basic" label="Confirm Password" variant="outlined" />
+
+          <Button className={styles.button} variant="contained">Create Account</Button>
+        </form>
+      </SignComponent>
     </div>
   );
 }
