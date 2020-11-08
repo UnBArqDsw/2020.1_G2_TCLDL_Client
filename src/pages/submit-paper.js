87ac756f0f2b08b1c1ec8a083f5bb32ae5b91d08
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2020-11-08 19:18:42 -0300

    add text to submit-paper page
    Co-authored-by: Mikhaelle <mikhabueno@outlook.com>

--- src/pages/submit-paper.js
+++ src/pages/submit-paper.js
@@ -1,17 +1,40 @@
-import Head from 'next/head';
 
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-
-import useStyles from '../styles/CreateAccount.module';
-
-import SignComponent from '../components/Sign'
 import AppBar from '../components/AppBar'
-export default function SubmitPaper() {
+import { Container } from '@material-ui/core';
+import style from '../styles/Submit-paper.module.css'
+import Grid from '@material-ui/core/Grid';
 
+export default function SubmitPaper() {
   return (
     <div>
       <AppBar />
+      <Container>
+        <div className={style.boxTitle}>
+          <div className={style.title}>
+            Submit paper
+          </div>
+        </div>
+      
+    
+      <div className={style.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+          <div className={style.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sed elementum tempus egestas. Ullamcorper velit sed ullamcorper morbi. Interdum posuere lorem ipsum dolor. At imperdiet dui accumsan sit. Diam quam nulla porttitor massa id neque aliquam. Eleifend quam adipiscing vitae proin sagittis nisl. Vel orci porta non pulvinar. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel. Mauris nunc congue nisi vitae suscipit tellus mauris a. Vulputate eu scelerisque felis imperdiet proin. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Elit ut aliquam purus sit amet luctus venenatis lectus. Sed cras ornare arcu dui vivamus arcu felis. Ut enim blandit volutpat maecenas. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Massa eget egestas purus viverra accumsan. Vitae et leo duis ut
+          </div>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <form className={styles.form} noValidate autoComplete="off">
+              <TextField id="filled-basic" label="Email" variant="outlined" />
+              <TextField id="outlined-basic" label="Password" variant="outlined" />
+            </form>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            oi
+          </Grid>
+        </Grid>
+      </div>
+      </Container>
     </div>
   );
 }
