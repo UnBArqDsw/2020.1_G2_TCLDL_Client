c7e9a58b008244bd2666bde2462003fba057bdc0
Mikhaelle Bueno <mikhabueno@outlook.com>
2020-11-08 23:48:55 -0300

    add profile grid
    Co-authored-by: Vitor <vitorrr.meireles@hotmail.com>

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,36 +1,29 @@
 import AppBar from '../components/AppBar'
 import { Container } from '@material-ui/core';
-import style from '../styles/Submit-paper.module.css'
 import useStyles from '../styles/components/SubmitPaper.module';
 import Button from '@material-ui/core/Button';
-import GridPapers from '../components/SubmitPaper'
+import Grid from '@material-ui/core/Grid';
+import style from '../styles/Profile.module.css';
 
 export default function SubmitPaper() {
-  const styles = useStyles();
-  return (
-    <div>
-      <AppBar />
-      <Container>
-        <div className={style.boxTitle}>
-          <div className={style.title}>
-            Submit paper
-          </div>
+    const styles = useStyles();
+    return (
+        <div>
+            <AppBar />
+            <Container>
+                <Grid container spacing={0} >
+                    <Grid item xs={12} sm={3}>
+                        <div className={style.boxTitle}>
+                            <div className={style.titleText}>
+                                Profile
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={9}>
+                        oi
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
-
-        <div className={style.root}>
-          <GridPapers text="" fieldText1="Paper Name" fieldText2="Author(s)" />
-          <GridPapers text="Lorem ipsum dolor sit amet" fieldText1="Release Date" fieldText2="Domain" />
-          <GridPapers text="consectetur adipiscing elit," fieldText1="Data Set" fieldText2="Model Name" />
-          <GridPapers text="sed do eiusmod tempor incididunt ut" fieldText1="Acurracy Type" fieldText2="Acurracy" />
-          <GridPapers text="labore et dolore magna aliqua" fieldText1="CPU GFlops" fieldText2="TPU GFlops" />
-          <GridPapers text="Massa sed elementum tempus egestas" fieldText1="#Of Parameters" fieldText2="Multiply-Adds" />
-          <GridPapers text="Ullamcorper velit sed ullamcorper morbi" fieldText1="Training Time" fieldText2="Dataset Size" />
-        </div>
-
-        <div className={style.button}>
-          <Button className={styles.button} variant="contained"  >SUBMIT PAPER</Button>
-        </div>
-      </Container>
-    </div>
-  );
+    );
 }
