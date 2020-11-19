6f4a22514db060b04e02d606a4c47682cdac66f3
Vitor Meireles Oliveira <vitorrr.meireles@hotmail.com>
2020-11-19 01:01:32 -0300

    Improve style submission-profile
    Co-authored-by: Mikhelle <mikhabueno@outlook.com>

--- src/pages/submission-profile.js
+++ src/pages/submission-profile.js
@@ -1,47 +1,47 @@
-import AppBar from '../components/AppBar'
-import { Container } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import useStyles from '../styles/Profile.module.js';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
-import TextField from '@material-ui/core/TextField';
-import { NaturePeopleOutlined } from '@material-ui/icons';
-import Link from 'next/link'
-import Table from '../components/SubmissionTable'
+import AppBar from "../components/AppBar";
+import { Container } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import useStyles from "../styles/Profile.module.js";
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import TextField from "@material-ui/core/TextField";
+import { NaturePeopleOutlined } from "@material-ui/icons";
+import Link from "next/link";
+import Table from "../components/SubmissionTable";
 
 export default function SubmissionProfile() {
-    const style = useStyles();
+  const style = useStyles();
 
-    return (
-        <div>
-            <AppBar />
-            <Container>
-            <Grid container spacing={10} >
-                    <Grid item xs={12} sm={3}>
-                        <div style={{ paddingLeft: '20px', borderRight: '1px solid #eaeaea' }}>
-                            <div className={style.boxTitle}>
-                                <div className={style.titleText}>
-                                    Submission
-                            </div>
-                            </div>
-                            <div>
-                                <Link href="/profile">
-                                    <Button className={style.buttonText1} >Profile</Button>
-                                </Link>
-                            </div>
-                            <div >
-                                <Link href="/review-profile">
-                                    <Button className={style.buttonText1} >Review</Button>
-                                </Link>
-                            </div>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm={9}>
-                        <Table/>
-                    </Grid>
-                </Grid>
-            </Container>
-        </div>
-    );
+  return (
+    <div>
+      <AppBar />
+      <Container>
+        <Grid container spacing={10}>
+          <Grid item xs={12} sm={3}>
+            <div
+              style={{ paddingLeft: "20px", borderRight: "1px solid #eaeaea" }}
+            >
+              <div className={style.boxTitle}>
+                <div className={style.titleText}>Submission</div>
+              </div>
+              <div>
+                <Link href="/profile">
+                  <Button className={style.buttonText1}>Profile</Button>
+                </Link>
+              </div>
+              <div>
+                <Link href="/review-profile">
+                  <Button className={style.buttonText1}>Review</Button>
+                </Link>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <Table />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
