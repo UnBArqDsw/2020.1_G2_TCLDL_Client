652d6449ce44c27bc92b22caa5d98f5b5293fec6
Guilherme Deusdará <guibanci@gmail.com>
2020-11-19 03:14:24 -0300

    ajusting domain item
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>

--- src/components/Home/Domain.js
+++ src/components/Home/Domain.js
@@ -4,8 +4,8 @@ import styles from '../../styles/components/Home/Domain.module.css'
 const Domain = ({ name, description }) => {
   return (
     <div className={styles.domain}>
-      <h4>{name}</h4>
-      <p>{description}</p>
+      <h4 className={styles.domainsBoard} >{name}</h4>
+      <p className={styles.domainsBoard} >{description}</p>
     </div>
   )
 }
