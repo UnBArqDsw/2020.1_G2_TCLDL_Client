1bd83dfa3d36454ebf4f636a0039e50df8690754
lorryaze <lorrany90@gmail.com>
2020-11-18 22:10:47 -0300

    feat: make images clickable

--- src/components/DomainContainer.js
+++ src/components/DomainContainer.js
@@ -30,141 +30,170 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function DomainContainer() {
   const classes = useStyles();
 
   return (
-
-      <Container >
-        <div>
-          <h1 style={{ fontFamily: 'Roboto', marginLeft: "200px"}}>Domain</h1>
-        </div>
-        <div>
-          <p style={{ fontFamily: 'Roboto', marginLeft: "200px",  marginTop: "50px"}}>06 domains available</p>
-        </div>
-        <div className={classes.root} style={{marginLeft: "200px"}} >
-        <hr  size="1px" width="73%" align="left"  noshade style={{marginBottom: "50px"}}></hr>
-          <Grid container spacing={8}>
-            <Grid container item xs={3}>
-              <Paper className={classes.paper}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid
-                    item
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      Machine Translation
-                    </Typography>
-                    <ButtonBase className={classes.image}>
+    <Container>
+      <div>
+        <h1 style={{ fontFamily: "Roboto", marginLeft: "200px" }}>Domain</h1>
+      </div>
+      <div>
+        <p
+          style={{
+            fontFamily: "Roboto",
+            marginLeft: "200px",
+            marginTop: "50px",
+          }}
+        >
+          06 domains available
+        </p>
+      </div>
+      <div className={classes.root} style={{ marginLeft: "200px" }}>
+        <hr
+          size="1px"
+          width="73%"
+          align="left"
+          noshade
+          style={{ marginBottom: "50px" }}
+        ></hr>
+        <Grid container spacing={8}>
+          <Grid container item xs={3}>
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid
+                  item
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    Machine Translation
+                  </Typography>
+                  <ButtonBase className={classes.image}>
+                    <a href="#" target="_blank">
                       <img src="/machine.png" alt="logo" />
-                    </ButtonBase>
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>2 Benchmarks</Typography>
-                  </Grid>
+                    </a>
+                  </ButtonBase>
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    2 Benchmarks
+                  </Typography>
                 </Grid>
-              </Paper>
-            </Grid>
-            <Grid container item xs={3}>
-              <Paper className={classes.paper}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid
-                    item
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      Image Classification
-                    </Typography>
-                    <ButtonBase className={classes.image}>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid container item xs={3}>
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid
+                  item
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    Image Classification
+                  </Typography>
+                  <ButtonBase className={classes.image}>
+                    <a href="#" target="_blank">
                       <img src="/classification.png" alt="logo" />
-                    </ButtonBase>
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      2 Benchmarks
-                    </Typography>
-                  </Grid>
+                    </a>
+                  </ButtonBase>
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    2 Benchmarks
+                  </Typography>
                 </Grid>
-              </Paper>
-            </Grid>
-            <Grid container item xs={3}>
-              <Paper className={classes.paper}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid
-                    item
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      Qustion Answering
-                    </Typography>
-                    <ButtonBase className={classes.image}>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid container item xs={3}>
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid
+                  item
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    Qustion Answering
+                  </Typography>
+                  <ButtonBase className={classes.image}>
+                    <a href="#" target="_blank">
                       <img src="/question.png" alt="logo" />
-                    </ButtonBase>
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      2 Benchmarks
-                    </Typography>
-                  </Grid>
+                    </a>
+                  </ButtonBase>
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    2 Benchmarks
+                  </Typography>
                 </Grid>
-              </Paper>
-            </Grid>
+              </Grid>
+            </Paper>
           </Grid>
-        </div>
-        <div className={classes.root} style={{ marginLeft: "200px", marginBottom: "100px" }}>
-          <Grid container spacing={8}>
-            <Grid container item xs={3} style={{ marginLeft: "150px"}}>
-              <Paper className={classes.paper}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid
-                    item
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
+        </Grid>
+      </div>
+      <div
+        className={classes.root}
+        style={{ marginLeft: "200px", marginBottom: "100px" }}
+      >
+        <Grid container spacing={8}>
+          <Grid container item xs={3} style={{ marginLeft: "150px" }}>
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid
+                  item
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Typography
+                    style={{ fontFamily: "Roboto Mono", marginLeft: "45px" }}
                   >
-                    <Typography style={{ fontFamily: "Roboto Mono", marginLeft: "45px" }}>
-                      Named Entity Recognition
-                    </Typography>
-                    <ButtonBase className={classes.image}>
+                    Named Entity Recognition
+                  </Typography>
+                  <ButtonBase className={classes.image}>
+                    <a href="#" target="_blank">
                       <img src="/named.png" alt="logo" />
-                    </ButtonBase>
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      2 Benchmarks
-                    </Typography>
-                  </Grid>
+                    </a>
+                  </ButtonBase>
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    2 Benchmarks
+                  </Typography>
                 </Grid>
-              </Paper>
-            </Grid>
-            <Grid container item xs={3}>
-              <Paper className={classes.paper}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid
-                    item
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                  >
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      Object Detection
-                    </Typography>
-                    <ButtonBase className={classes.image}>
-                      <img src="/object.png" alt="logo" />
-                    </ButtonBase>
-                    <Typography style={{ fontFamily: "Roboto Mono" }}>
-                      2 Benchmarks
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
+              </Grid>
+            </Paper>
           </Grid>
-        </div>
-      </Container>
+          <Grid container item xs={3}>
+            <Paper className={classes.paper}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid
+                  item
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    Object Detection
+                  </Typography>
+                  <ButtonBase className={classes.image}>
+                    <a href="#" target="_blank">
+                      <img src="/object.png" alt="logo" />
+                    </a>
+                  </ButtonBase>
+                  <Typography style={{ fontFamily: "Roboto Mono" }}>
+                    2 Benchmarks
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
   );
 }
