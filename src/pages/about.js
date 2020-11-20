8d574f68d8f9b565334c2f7bb3b3b4bd3b526a94
Gabriel <gabrielfilipe7unb@gmail.com>
2020-11-20 15:13:23 -0300

    Fix about page appbar
    Signed-off-by: Gabriel <gabrielfilipe7unb@gmail.com>
    Co-authored-by: Guilherme Banci <guibanci@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,10 @@
 import React from "react";
-import AppBar from "../components/AppBar";
 import AboutContainer from "../components/AboutContainer";
 
 
 export default function About() {
   return (
     <div>
-      <AppBar />
       <AboutContainer />
     </div>
   );
