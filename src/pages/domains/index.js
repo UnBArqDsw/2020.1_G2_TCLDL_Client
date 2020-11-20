cb1eb11a5530ec3e8a43bcd133d2173eb5d4a3a3
Gabriel <gabrielfilipe7unb@gmail.com>
2020-11-20 15:14:33 -0300

    Fix domians route
    Signed-off-by: Gabriel <gabrielfilipe7unb@gmail.com>
    Co-authored-by: Guilherme Banci <guibanci@gmail.com>

--- src/pages/domains/index.js
+++ src/pages/domains/index.js
@@ -1,14 +1,12 @@
 // Imports
 import { Container } from "@material-ui/core";
 import React from "react";
-import AppBar from "../components/AppBar";
-import DomainContainer from "../components/DomainContainer";
-import styles from '../styles/Home.module.css';
+import DomainContainer from "../../components/DomainContainer";
+import styles from '../../styles/Home.module.css';
 
 export default function Domain() {
   return (
     <div>
-      <AppBar />
       <Container>
         <DomainContainer />
       </Container>
