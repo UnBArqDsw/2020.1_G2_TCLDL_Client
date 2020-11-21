254b0bfba2c038aaceba11bf4a5471a61e9173de
Guilherme Deusdará <guibanci@gmail.com>
2020-11-20 23:01:35 -0300

    Ajusting submit paper
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>
    Co-authored-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>

--- src/pages/submit-paper.js
+++ src/pages/submit-paper.js
@@ -5,7 +5,7 @@ import RemoveIcon from '@material-ui/icons/Remove';
 import AddIcon from '@material-ui/icons/Add';
 import { error } from 'highcharts';
 import useStyles from '../styles/components/SubmitPaper.module';
-
+import axios from 'axios';
 
 export default function SubmitPaper() {
   const styles = useStyles();
@@ -45,7 +45,7 @@ export default function SubmitPaper() {
   }; 
 
   const handleAccuracyTypeChange = (event) => {
-    setDataset(event.target.value);
+    setAccuracyType(event.target.value);
   };
 
   const handleCpuModelChange = (event) => {
@@ -61,16 +61,17 @@ export default function SubmitPaper() {
   };
 
 
-  const toggle = (name, func, value) => (
-    <FormControl required className={styles.selectField}>
+  const toggle = (name, func, value, values) => (
+    <FormControl disabled={!values} className={styles.selectField}>
       <InputLabel>{name}</InputLabel>
       <Select
         value={value}
         onChange={func}
       >
-        <MenuItem value={10}>Ten</MenuItem>
-        <MenuItem value={20}>Twenty</MenuItem>
-        <MenuItem value={30}>Thirty</MenuItem>
+
+        {values ? values.map((item) => (
+          <MenuItem value={item}>{item}</MenuItem>
+        )) : null}
       </Select>
     </FormControl>
   )
@@ -93,16 +94,79 @@ export default function SubmitPaper() {
 
   const onSubmit = () => {
     setSubmitted(true)
-    console.log('Foi aqui')
+    if (condition) {
+      
+    }
+
+    // {
+    //   "name": "valid_name",
+    //   "author": "valid_author",
+    //   "dateSubmission": "2020/10/10",
+    //   "link": "http://google.com",
+    //   "codeLink": "http://google.com",
+    //   "domain": "valid_domain",
+    //   "dataset": "valid_dataset",
+    //   "modelName": "valid_modelName",
+    //   "accuracyInformation": {
+    //     "accuracy": "10.10"
+    //   },
+    //   "modelInformation": {
+    //     "trainingTime": "10",
+    //     "trainingDataSize": "valid_trainingDataSize",
+    //     "epochs": "valid_epochs"
+    //   },
+    //   "hardwareInformation": {
+    //     "cpuModel": "valid_cpuModel",
+    //     "cpuCores": 1,
+    //     "cpuGflops": "valid_cpuGflops",
+    //     "gpuModel": "valid_gpuModel",
+    //     "gpuCores": 1,
+    //     "gpuGflops": "valid_gpuGflops",
+    //     "tpuModel": "valid_tpuModel",
+    //     "tpuCores": 1,
+    //     "tpuGflops": "valid_tpuGflops"
+    //   }
+    // }
+    
+  }
+
+  const onChangeAuthor = (event, index) => {
+    let newAuthors = authorsField
+    newAuthors[index] = event.target.value
+    setAuthorsField([...newAuthors])
   }
 
   const authorField = () => {
     return  authorsField.map((el, index) => 
       <div key={index}>
-        <TextField className={styles.authorField} required id="outlined-basic" label="Author(s)" />
+        <TextField onChange={(event) => onChangeAuthor(event, index)} className={styles.authorField} required id="outlined-basic" label="Author" />
       </div>
     )
   }
+
+  const domainList = [
+    "IMAGE CLASSIFICATION",
+    "OBJECT DETECTION",
+    "QUESTION ANSWERING",
+    "NAMED ENTITY RECOGNITION",
+    "MACHINE TRANSLATION"
+  ]
+
+  const datasetsList = [
+    "imagenet",
+    "mscoco",
+    "squad1_1",
+    "conll2003",
+    "wmt2014_enfr",
+    "wmt2014_enge"
+  ]
+
+  const accuracyTypes = [
+    'TOP 1',
+    'BOX AP',
+    'F1 SCORE',
+    'BLEU'
+  ]
 
   return (
     <div>
@@ -145,8 +209,8 @@ export default function SubmitPaper() {
                   <TextField required error={submitted && !codeLink.length ? true : false} helperText={submitted && !codeLink.length ? "Required Field" : ""}  className={styles.flexLinks} id="outlined-basic" label="Code Link"onChange={handleCodeLink}/>
                 </div>
                 <div>
-                  {toggle('Domain', handleDomainChange, domain)}
-                  {toggle('Dataset', handleDatasetChange, dataset)}
+                  {toggle('Domain', handleDomainChange, domain, domainList)}
+                  {toggle('Dataset', handleDatasetChange, dataset, datasetsList)}
                   <TextField required error={submitted && !modelName.length ? true : false} helperText={submitted && !modelName.length ? "Required Field" : ""} className={styles.field} id="outlined-basic" label="Model Name" onChange={handleModelName}/>
                 </div>
               </div>
@@ -157,7 +221,7 @@ export default function SubmitPaper() {
             <h2>Accuracy information</h2>
             <div>
               <div>
-                {toggle('Accuracy Type', handleAccuracyTypeChange, accuracyType)}
+                {toggle('Accuracy Type', handleAccuracyTypeChange, accuracyType, accuracyTypes)}
                 <TextField required className={styles.field} id="outlined-basic" label="Accuracy" />
               </div>
             </div>
