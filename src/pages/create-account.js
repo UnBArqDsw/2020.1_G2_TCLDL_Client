0cfda3c297c321fd9dd7a3d728395d60c6b6b0d2
Gabriel Filipe <gabrielfilipe7unb@gmail.com>
2020-11-20 22:59:43 -0300

    Ajusting create-account
    Signed-off-by: Gabriel Filipe <gabrielfilipe7unb@gmail.com>
    Co-authored-by: Guilherme Banci <guibanci@gmail.com>

--- src/pages/create-account.js
+++ src/pages/create-account.js
@@ -5,9 +5,14 @@ import { toast } from 'react-toastify';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as userAction } from '../store/reducer';
+
 import useStyles from '../styles/components/CreateAccount.module';
 import SignComponent from '../components/Sign';
-export default function Home() {
+
+function CreateAccount({ setUser }) {
   const styles = useStyles();
 
   const [name, setName] = useState('');
@@ -24,12 +29,23 @@ export default function Home() {
   }
 
 
-  const onSubmit = () => {
-    axios.post('http://localhost:3001/v0/signUp', { name, lastName, email, password, passwordConfirmation })
-      .then(() => router.push('/login'))
-      .catch(error => {
-        toast(error)
-      })
+  const onSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/v0/signUp', { name, lastName, email, password, passwordConfirmation })
+      console.log(response)
+      if (response && response.data) {
+        const response2 = await axios.post('http://localhost:3001/v0/login', { email, password })
+        console.log(response2)
+        if (response2.data) {
+          setUser({ ...response.data, ...response2.data })
+          router.push('/')
+        }
+        // setUser({ ...data, token: `Bearer ${token}` })
+      }
+    } catch (error) {
+      console.log(error)
+      toast(error)
+  }
   }
 
   return (
@@ -48,3 +64,11 @@ export default function Home() {
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(userAction, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateAccount);
